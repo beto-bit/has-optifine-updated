8c165dd42c08941b971310c63e69a870926f60fd
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-14 20:14:12 -0600

    Working fetching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use select::{document::Document, predicate::{Class, Name}};
+
+const URL: &str = "https://www.optifine.net/downloads";
+
 fn main() {
-    println!("Hello, world!");
+    let response = fetch_url(URL);
+    let version = extract_version(&response).unwrap();
+
+    println!("Current OptiFine version: {}", version);
+}
+
+fn fetch_url(url: &str) -> String {
+    reqwest::blocking::get(url)
+        .expect("Couldn't fetch data")
+        .text()
+        .expect("Error decoding data")
+}
+
+fn extract_version(html: &str) -> Option<String> {
+    let document = Document::from(html);
+    let html_span = document.find(Class("downloads")).nth(0)?;
+    let html_h2 = html_span.find(Name("h2")).nth(0)?;
+
+    Some(html_h2.text())
 }
