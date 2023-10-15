51707fc79066ef4091f34067dba6b72acaf75a94
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-14 20:40:24 -0600

    Just some clippy changes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use select::{document::Document, predicate::{Class, Name}};
+use select::document::Document;
+use select::predicate::{Class, Name};
 
 const URL: &str = "https://www.optifine.net/downloads";
 
 fn main() {
     let response = fetch_url(URL);
-    let version = extract_version(&response).unwrap();
+    let version = extract_version(&response).expect("Couldn't parse HTML");
 
-    println!("Current OptiFine version: {}", version);
+    println!("Current OptiFine version: {version}");
 }
 
 fn fetch_url(url: &str) -> String {
@@ -18,8 +19,8 @@ fn fetch_url(url: &str) -> String {
 
 fn extract_version(html: &str) -> Option<String> {
     let document = Document::from(html);
-    let html_span = document.find(Class("downloads")).nth(0)?;
-    let html_h2 = html_span.find(Name("h2")).nth(0)?;
+    let html_span = document.find(Class("downloads")).next()?;
+    let html_h2 = html_span.find(Name("h2")).next()?;
 
     Some(html_h2.text())
 }
