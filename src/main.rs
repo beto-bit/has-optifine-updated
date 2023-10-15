442e1884d3dda9d861c615c92266bfc8d2ed6e83
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-10-14 21:34:57 -0600

    Over-engineering

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::path::Path;
 
 use select::document::Document;
 use select::predicate::{Class, Name};
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 const URL: &str = "https://www.optifine.net/downloads";
 
 #[tokio::main]
 async fn main() {
-    let response = fetch_url(URL).await;
+    // Fetch web and data at the same time
+    let (response, cached_data) = tokio::join!(fetch_url(URL), load_cached_data("Cargo.toml"));
+
+    // Extract the version string "Minecraft 1.xx.xx"
     let version = extract_version(&response).expect("Couldn't parse HTML");
 
     println!("Current OptiFine version: {version}");
+    println!("Data: {}", cached_data.unwrap());
 }
 
 async fn fetch_url(url: &str) -> String {
@@ -30,10 +36,20 @@ fn extract_version(html: &str) -> Option<String> {
     Some(html_h2.text())
 }
 
-fn load_cached_data(path: impl AsRef<Path>) -> Option<String> {
-    todo!()
+async fn load_cached_data(path: impl AsRef<Path>) -> Option<String> {
+    let mut file = File::open(path).await.ok()?;
+
+    // Read the entire file to a vector, then to a String
+    let mut buff = vec![];
+    file.read_to_end(&mut buff).await.ok()?;
+    let contents = String::from_utf8(buff).expect("Invalid UTF-8");
+
+    Some(contents)
 }
 
-fn store_data(data: &str, path: impl AsRef<Path>) -> std::io::Result<()> {
-    todo!()
+async fn store_data(data: &str, path: impl AsRef<Path>) -> std::io::Result<()> {
+    let mut file = File::create(path).await?;
+    file.write_all(data.as_bytes()).await?;
+
+    Ok(())
 }
